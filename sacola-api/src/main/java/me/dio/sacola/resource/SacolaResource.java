287a9e9a60f1353c46f377bb959a413cd105e942
USUARIO <jucemeirelopes@gmail.com>
2022-09-30 23:09:33 -0300

    Commit aula 3

---
+++ sacola-api/src/main/java/me/dio/sacola/resource/SacolaResource.java
@@ -0,0 +1,45 @@
+package me.dio.sacola.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import me.dio.sacola.model.Item;
+import me.dio.sacola.model.Sacola;
+import me.dio.sacola.service.SacolaService;
+
+@RestController
+@RequestMapping("/ifood-devweek/sacolas")
+@RequiredArgsConstructor
+public class SacolaResource {
+	
+	@Autowired
+	private final SacolaService sacolaService;
+	
+	@PostMapping
+	public Item incluirItemNaSacola(@RequestBody ItemDto itemDto) {
+		return sacolaService.incluirItemNaSacola(itemDto);
+	}
+	
+	@GetMapping("/{id}")
+	public Sacola verSacola(@PathVariable("id") Long id) {
+		return sacolaService.verSacola(id);
+	}
+	
+	
+	@PatchMapping("/fecharSacola/{sacolaId}")
+	public Sacola fecharSacola(@PathVariable("sacolaId") Long sacolaId,
+			@RequestParam("formaPagamento") int formaPagamento) {
+		return sacolaService.fecharSacola(sacolaId, formaPagamento);
+	}
+	
+	}
+
+
---
+++ sacola-api/src/main/java/me/dio/sacola/service/impl/SacolaServiceImpl.java
@@ -0,0 +1,92 @@
+package me.dio.sacola.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import me.dio.sacola.enumeration.FormaPagamento;
+import me.dio.sacola.model.Item;
+import me.dio.sacola.model.Restaurante;
+import me.dio.sacola.model.Sacola;
+import me.dio.sacola.repository.ItemRepository;
+import me.dio.sacola.repository.ProdutoRepository;
+import me.dio.sacola.repository.SacolaRepository;
+import me.dio.sacola.resource.ItemDto;
+import me.dio.sacola.service.SacolaService;
+
+@RequiredArgsConstructor
+@Service
+public class SacolaServiceImpl implements SacolaService {
+
+	@Autowired
+	private final SacolaRepository sacolaRepository;
+	
+	@Autowired
+	private final ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private final ItemRepository itemRepository;
+	
+	@Override
+	public Sacola verSacola(Long id) {
+		return sacolaRepository.findById(id).orElseThrow (() -> {
+			throw new RuntimeException("Essa sacola não existe!");
+		});
+	}
+
+	@Override
+	public Sacola fecharSacola(Long id, int numeroformaPagamento) {
+		Sacola sacola = verSacola(id);
+		if(sacola.getItens().isEmpty()) {
+			throw new RuntimeException("Inclua ítens na sacola!");
+		} 
+		
+		FormaPagamento formaPagamento =  numeroformaPagamento == 0 ? FormaPagamento.DINHEIRO : FormaPagamento.DINHEIRO;
+		
+		sacola.setFormaPagamento(formaPagamento);
+		sacola.setFechada(true);
+		sacolaRepository.save(sacola);
+		return sacola;
+	}
+
+	@Override
+	public Item incluirItemNaSacola(ItemDto itemDto) {
+		Sacola sacola = verSacola(itemDto.getSacolaId());
+		
+		
+		
+		if(sacola.isFechada()) {
+			throw new RuntimeException("Esta sacola está fechada!");
+
+		}
+		Item itemParaSerInserido =  Item.builder()
+			.quantidade(itemDto.getQuantidade())
+			.sacola(sacola)
+			.produto(produtoRepository.findById(itemDto.getProdutoId()).orElseThrow(()
+					-> {
+						throw new RuntimeException("Esse produto não existe!");
+
+					}))
+			.build();
+		
+		List<Item> itensDaSacola = sacola.getItens();
+		if(itensDaSacola.isEmpty()) {
+			itensDaSacola.add(itemParaSerInserido);
+		} else {
+			Restaurante restauranteAtual = itensDaSacola.get(0).getProduto().getRestaurante();
+			Restaurante restauranteDoItemParaAdicionar =  itemParaSerInserido.getProduto().getRestaurante();
+			if(restauranteAtual.equals(restauranteDoItemParaAdicionar)) {
+				itensDaSacola.add(itemParaSerInserido);
+			} else {
+				throw new RuntimeException("Não é possível adicionar produtos de restaurantes diferentes. Feche a sacola ou esvazie!");
+
+			}
+		}
+		sacolaRepository.save(sacola);
+
+		return itemParaSerInserido;
+	}
+
+}
